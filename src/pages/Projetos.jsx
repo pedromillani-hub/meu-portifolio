7b76072161baa37b4ee66151d3e8a63d2fe98b0a
pedromillani-hub <pedro_millani@estudante.sesisenai.org.br>
2026-09-24 10:39:59 -0300

    Update Projetos.jsx

--- src/pages/Projetos.jsx
+++ src/pages/Projetos.jsx
@@ -4,21 +4,73 @@ function Projetos() {
       <h1>Meus Projetos</h1>
 
       <div className="projeto-card">
-        <h2>Projeto 1 — Sistema de Biblioteca</h2>
-        <p>Sistema para controle de livros e usuários.</p>
-        <p><strong>Tecnologias:</strong> HTML, CSS e JavaScript</p>
+        <h2>Projeto 1 — Jogo de Xadrez blitz</h2>
+        <p>Jogo estratégico</p>
+         <p><strong>Tecnologias:</strong> HTML, CSS e JavaScript</p>
+        <a
+          href="https://xadrez-three-nu.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            display: "inline-block",
+            backgroundColor: "#3b82f6",
+            color: "white",
+            padding: "0.75rem 1.5rem",
+            borderRadius: "8px",
+            textDecoration: "none",
+            fontWeight: 500,
+            marginTop: "1rem"
+          }}
+        >
+          Ver Projeto
+        </a>
+        
       </div>
 
       <div className="projeto-card">
-        <h2>Projeto 2 — Lista de Tarefas</h2>
-        <p>Aplicação para organização de tarefas diárias.</p>
-        <p><strong>Tecnologias:</strong> React e JavaScript</p>
+        <h2>Projeto 2 — Trino para minha mãe</h2>
+        <p>lista de exercício da minha pra minha</p>
+        <p><strong>Tecnologias:</strong> JavaScript, html e css</p>
+        <a 
+  href="https://treino-jaqueline-rust.vercel.app" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  style={{
+    display: "inline-block",
+    backgroundColor: "#3b82f6",
+    color: "white",
+    padding: "0.75rem 1.5rem",
+    borderRadius: "8px",
+    textDecoration: "none",
+    fontWeight: 500,
+    marginTop: "1rem"
+  }}
+>
+  Ver Projeto
+</a>
       </div>
 
       <div className="projeto-card">
-        <h2>Projeto 3 — Loja Virtual</h2>
-        <p>Protótipo de um e-commerce interativo.</p>
-        <p><strong>Tecnologias:</strong> React e CSS</p>
+        <h2>Projeto 3 — barbearia chamada hd</h2>
+        <p>um site com tabelas de preço</p>
+        <p><strong>Tecnologias:</strong> HTML e CSS</p>
+        <a 
+  href="https://treino-jaqueline-rust.vercel.app" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  style={{
+    display: "inline-block",
+    backgroundColor: "#3b82f6",
+    color: "white",
+    padding: "0.75rem 1.5rem",
+    borderRadius: "8px",
+    textDecoration: "none",
+    fontWeight: 500,
+    marginTop: "1rem"
+  }}
+>
+  Ver Projeto
+</a>
       </div>
     </div>
   );
